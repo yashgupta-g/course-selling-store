a14ffac602df9f8b530aee86b0e54d88eab7d539
yash <52443096+yashgupta-g@users.noreply.github.com>
2023-08-08 03:52:26 +0530

    project setup and footer aaded

---
+++ src/components/RESUABLE/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+
+<div className=' footer-block'>
+<div className=' container-fluid '>
+<div className=' row '>
+    <div className=' col-12 pt-2 mx-auto bg-dark text-center text-light'>
+    <h6>
+    Copyright footer :2023
+</h6>
+
+
+    </div>
+
+
+</div>
+</div>
+
+
+</div>
+
+    </footer>
+  )
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,45 +6,18 @@
  */
 
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import "./bootstrap.min.css"
 import "./layout.css"
-
+import Footer from "./RESUABLE/Footer"
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
+    <div>
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        <Footer>
+          
+        </Footer>
       </div>
-    </>
   )
 }
 
