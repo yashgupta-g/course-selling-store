8a1518596a06cd1848b99e56f69d0420636e4118
yash gupta <14yashapps@gmail.com>
2023-08-14 15:24:18 +0530

    ssr added

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,16 +13,20 @@ require('dotenv').config({
 })
 module.exports = {
 
-  flags: {
-    DEV_SSR: true,
-  },
   
+
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
+
+  flags: {
+    DEV_SSR: true,
+  },
+
+  
   plugins: [
     `gatsby-plugin-image`,
     {
